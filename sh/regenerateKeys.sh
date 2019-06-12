3f650ccc25b7ec337f5758e7800433c0b4b13094
Varga Máté <varga.mate.unideb@gmail.com>
2019-06-12 09:12:09 +0200

    Key generation concatenation and line endings fix.

--- sh/regenerateKeys.sh
+++ sh/regenerateKeys.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 openssl genrsa -out $(dirname $0)/rsa_key.pem 4096
 openssl rsa -pubout -in $(dirname $0)/rsa_key.pem -out $(dirname $0)/rsa_pub.pem
-echo "var pub=\"" > $(dirname $0)/../js/pub.js;
-cat $(dirname $0)/rsa_pub.pem >> $(dirname $0)/../js/pub.js
-echo "\"" >> $(dirname $0)/../js/pub.js;
-
+echo "var pub=" > $(dirname $0)/../js/pub.js;
+cat $(dirname $0)/rsa_pub.pem | sed "s|^|\"|g" | sed "s|$|\\\n\"+|g" >> $(dirname $0)/../js/pub.js
+echo "\"\"" >> $(dirname $0)/../js/pub.js;
